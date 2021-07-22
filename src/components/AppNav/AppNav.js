480dbfd92a1c058a08c003782f154b2b8b3b7b6f
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-22 16:22:13 -0400

    minor fix

--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -4,11 +4,9 @@ function AppNav(props) {
     const renderNavItems = () => {
     let elements = props.navItems.map((item, index) => {
       return (
-        <div>
           <a href='#' key={index} onClick={ () => props.handleNavClick(item.value) }> 
           | { item.label } | 
           </a> 
-        </div>
       )
     })
     return <span>
