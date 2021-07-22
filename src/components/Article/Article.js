71452645ed349ba4ed41313981f7ace7c4ca003a
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-22 16:05:31 -0400

    functional article

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,32 +1,15 @@
-import React, { Component } from 'react';
 
-class Article extends Component {
-  render() {
-    const { title, created_date: createdDate, abstract, byline, image } = this.props;
-    return (
-      <div>
-        <h1>{ title }</h1>
-        <p>{ createdDate }</p>
-        { byline && <h2>{ byline }</h2> }
-        { this.props.multimedia[0] && <img alt={ title } src={ this.props.multimedia[0].url }/> }
-        <p>{ abstract }</p>
-      </div>
-    )
-  }
+
+function Article(props) {
+  return (
+    <div>
+      <h1> {props.title} </h1>
+      <p> {props.created_date} </p>
+      { props.byline && <h2> { props.byline }</h2>}
+      { props.multimedia[0] && <img alt={ props.title } src={ props.multimedia[0].url }/> }
+      <p> { props.abstract }</p>
+    </div>
+  )
 }
 
 export default Article;
-
-
-// Functional solution:
-// function Article({ title, created_date: createdDate, abstract, byline, image }) {
-//   return (
-//     <div>
-//       <h1>{ title }</h1>
-//       <p>{ createdDate }</p>
-//       { byline && <h2>{byline}</h2> }
-//       { image && <img src={image} /> }
-//       <p>{ abstract }</p>
-//     </div>
-//   );
-// }
--- src/components/ArticleTeaser/ArticleTeaser.js
+++ src/components/ArticleTeaser/ArticleTeaser.js
@@ -1,4 +1,5 @@
 
+
 function ArticleTeaser(props) {
   
   return (
@@ -16,16 +17,4 @@ function ArticleTeaser(props) {
   )
 }
 
-
 export default ArticleTeaser;
-
-// The `ArticleTeaser`
-// component should:
-//   1. Display the `title`
-// inside of an `<a>`
-// tag.
-// 2. When the `title`
-// `<a>`
-// tag is clicked, it should call `this.props.handleTitleClick(this.props.id);`.Will arrow functions be useful here ?
-//   3. Display the `created_date` in a `<p>`
-// tag.
