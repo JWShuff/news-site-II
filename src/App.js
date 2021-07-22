ca328c49d7fe5f6a6efc648ee3a7502582dc46bb
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-22 16:21:29 -0400

    functional complete

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ function App(props) {
       </Router>
     </div>
   );
-
 }
 
 export default App;
--- src/components/AppNav/AppNav.js
+++ src/components/AppNav/AppNav.js
@@ -4,9 +4,11 @@ function AppNav(props) {
     const renderNavItems = () => {
     let elements = props.navItems.map((item, index) => {
       return (
+        <div>
           <a href='#' key={index} onClick={ () => props.handleNavClick(item.value) }> 
           | { item.label } | 
           </a> 
+        </div>
       )
     })
     return <span>
