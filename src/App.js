8fb99b6ba4fa8f387bef137713f3114aa4c5e6c6
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-22 15:37:25 -0400

    Functionalized the App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import News from './data/news.json';
 import navItems from './data/navItems.json';
 import './App.css';
@@ -8,33 +8,25 @@ import ArticlePage from './pages/ArticlePage';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App(props) {
+  const [navItemsData, setNavItemsData] = useState(navItems);
 
-    this.state = {
-      navItems: navItems,
-    }
-  }
 
-  render() {
-    const { article, navItems } = this.state
+  return (
+    <div>
+      <hr />
+      <AppNav navItems={navItemsData} handleNavClick={(clickedItem) => { console.log(clickedItem) }} />
+      <Router>
+        <div>
+          <Route exact path='/' component={HomePage}/>
+        </div>
+        <div>
+          <Route exact path='/articles/:articleID' component={ArticlePage} />
+        </div>
+      </Router>
+    </div>
+  );
 
-    return (
-      <div>
-        <hr />
-        <AppNav navItems={navItems} handleNavClick={(clickedItem) => { console.log(clickedItem) }} />
-        <Router>
-          <div>
-            <Route exact path='/' component={HomePage}/>
-          </div>
-          <div>
-            <Route exact path='/articles/:articleID' component={ArticlePage} />
-          </div>
-        </Router>
-      </div>
-    );
-  }
 }
 
 export default App;
